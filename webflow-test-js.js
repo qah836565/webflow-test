4d71397b5bf5e12011f77430a7732c342296d075
qah836565 <qah83@usmail.one>
2026-09-15 19:36:03 +0530

    Update webflow-test-js.js

--- webflow-test-js.js
+++ webflow-test-js.js
@@ -82,7 +82,21 @@
 
             slidesToShow: 3,
 
-            slidesToScroll: 1
+            slidesToScroll: 1,
+             responsive: [
+                 {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                 {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+                 ]
 
         });
 
