d5f387410bb3470cfae3e3d6a8e3208661a76af3
qah836565 <qah83@usmail.one>
2026-09-15 12:05:48 +0530

    Update webflow-test-js.js

--- webflow-test-js.js
+++ webflow-test-js.js
@@ -1,4 +1,35 @@
-body{
-    color: #fff;
-    background: #000;
-}
+
+
+var $universitySlider = $('.university-slider');
+    var $progress = $('.univ-sld-progress-bar .univ-sld-progress-fill');
+
+    $universitySlider.on('init beforeChange', function (e, slick, _, next) {
+        var i = e.type === 'init' ? 0 : next;
+        $progress.width(((i + 1) / slick.slideCount) * 100 + '%');
+    });
+
+
+    $universitySlider.slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        prevArrow: '.univ-sld-btn-left',
+        nextArrow: '.univ-sld-btn-right',
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
